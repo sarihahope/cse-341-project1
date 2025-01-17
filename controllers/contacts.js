ed99ed1d097b1743500a291ce2bcc98b3c9444b3
sarihahope <sarihaslink@yahoo.com>
2025-01-16 18:06:23 -0800

    Added, add contact, delete contact and update contact.

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -18,7 +18,53 @@ const getSingle = async (req, res) => {
    });
 };
 
+const createContact = async (req, res) => {
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const result = await mongodb.getDatabase().db().collection('contacts').insertOne(contact);
+    if (result.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(result.error || 'Failed to create contact');
+    }
+};
+
+const updateContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const result = await mongodb.getDatabase().db().collection('contacts').replaceOne({_id: userId}, contact);
+    if (result.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(result.error || 'Failed to update contact');
+    }
+}
+
+const deleteContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('contacts').deleteOne({_id: userId});
+    if (result.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(result.error || 'Failed to delete contact');
+    }
+}
+
 module.exports = {
     getContacts,
-    getSingle
+    getSingle,
+    createContact,
+    updateContact,
+    deleteContact
 };
